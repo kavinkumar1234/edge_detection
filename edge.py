c50a5b34e3af6443eca830558b48f09ddd5a9e4a
kavinkumar1234 <86126996+kavinkumar1234@users.noreply.github.com>
2021-06-24 13:30:42 +0530

    Add files via upload

---
+++ edge.py
@@ -0,0 +1,20 @@
+import cv2
+import imutils
+import matplotlib
+
+img=cv2.imread(r"C:\Users\Parents Gift\Desktop\roboAI\edge_detect\jog.jpg")
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow("GrayImage", gray_img)
+cv2.waitKey(0)
+
+#Canny_1 = cv2.Canny(gray_img, 50,170)
+#cv2.imshow("mid",Canny_1)
+#cv2.waitKey(0)
+
+#Canny_2 = cv2.Canny(gray_img, 30,150)
+#cv2.imshow("low",Canny_2)
+#cv2.waitKey(0)
+
+Canny_3 = cv2.Canny(gray_img, 165,170)
+cv2.imshow("high",Canny_3)
+cv2.waitKey(0)
